d9705923d35b9f43315dc391c4548a904566f12c
Mahiro Sudo <oriham.odus@gmail.com>
2026-01-06 14:55:20 +0900

    Add: plot

--- kalman.py
+++ kalman.py
@@ -1,12 +1,12 @@
 import argparse
 import numpy as np
+import matplotlib.pyplot as plt
 
 def simulate_1d_robot(steps: int, seed: int, q: float, r: float):
     rng = np.random.default_rng(seed)
-
     # 入力（行動）: ±1
     u = rng.choice([-1.0, 1.0], size=steps)
-
+    #print(u) 
     x_true = np.zeros(steps, dtype=float)
     z_meas = np.zeros(steps, dtype=float)
 
@@ -23,6 +23,17 @@ def simulate_1d_robot(steps: int, seed: int, q: float, r: float):
 
     return u, x_true, z_meas
 
+def plot(t, x_true, z_meas):
+    plt.figure()
+    plt.plot(t, x_true, label="True position")
+    plt.plot(t, z_meas, label="Measurement", linestyle="None", marker="o", markersize=3)
+    plt.xlabel("time step")
+    plt.ylabel("position")
+    plt.title("True-Measurement")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--steps", type=int, default=60)
@@ -32,10 +43,8 @@ def main():
     args = parser.parse_args()
 
     u, x_true, z_meas = simulate_1d_robot(args.steps, args.seed, args.q, args.r)
-
-    print("u[:5]     =", u[:5])
-    print("x_true[:5]=", x_true[:5])
-    print("z_meas[:5]=", z_meas[:5])
+    t = np.arange(args.steps)
+    plot(t, x_true, z_meas)
 
 if __name__ == "__main__":
     main()
