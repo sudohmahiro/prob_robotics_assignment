ade8415014bd79716f5c26f745c8c3a18222d982
Mahiro Sudo <oriham.odus@gmail.com>
2026-01-07 14:44:10 +0900

    Add: Prediction

--- kalman.py
+++ kalman.py
@@ -23,13 +23,22 @@ def simulate_1d_robot(steps: int, seed: int, q: float, r: float):
 
     return u, x_true, z_meas
 
-def plot(t, x_true, z_meas):
+def predict(u, x0=0.0):
+    x=float(x0)
+    x_pred=np.zeros(len(u), dtype=float)
+    for t in range(len(u)):
+        x=x+u[t]
+        x_pred[t]=x
+    return x_pred
+
+def plot(t, x_true, z_meas, x_predict):
     plt.figure()
     plt.plot(t, x_true, label="True position")
     plt.plot(t, z_meas, label="Measurement", linestyle="None", marker="o", markersize=3)
+    plt.plot(t, x_predict, label="prediction")
     plt.xlabel("time step")
     plt.ylabel("position")
-    plt.title("True-Measurement")
+    plt.title("Prediction-True/Measurement")
     plt.legend()
     plt.tight_layout()
     plt.show()
@@ -40,11 +49,13 @@ def main():
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--q", type=float, default=0.2)
     parser.add_argument("--r", type=float, default=2.0)
+    parser.add_argument("--x0", type=float, default=0.0)
     args = parser.parse_args()
 
     u, x_true, z_meas = simulate_1d_robot(args.steps, args.seed, args.q, args.r)
+    x_predict = predict(u, x0=args.x0)
     t = np.arange(args.steps)
-    plot(t, x_true, z_meas)
+    plot(t, x_true, z_meas, x_predict)
 
 if __name__ == "__main__":
     main()
