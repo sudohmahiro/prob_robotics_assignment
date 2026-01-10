2269386f5c0958726495c690abb6ceaa347ad29c
Mahiro Sudo <oriham.odus@gmail.com>
2026-01-10 14:43:20 +0900

    Add: kalman filter

--- kalman.py
+++ kalman.py
@@ -23,22 +23,44 @@ def simulate_1d_robot(steps: int, seed: int, q: float, r: float):
 
     return u, x_true, z_meas
 
-def predict(u, x0=0.0):
-    x=float(x0)
-    x_pred=np.zeros(len(u), dtype=float)
-    for t in range(len(u)):
-        x=x+u[t]
-        x_pred[t]=x
-    return x_pred
+def kalman_filter(u, z, x0=0.0, p0=0.0, q=0.2, r=2.0):
+    steps=len(z)
+    x_est=np.zeros(steps, dtype=float)
+    p_est=np.zeros(steps, dtype=float)
+    k_hist=np.zeros(steps, dtype=float)
 
-def plot(t, x_true, z_meas, x_predict):
+    x=float(x0)
+    p=float(p0)
+
+    for t in range(steps):
+        x_pred=x+u[t]
+        p_pred=p+q
+
+        k=p_pred/(p_pred+r)   #カルマンゲイン
+        x=x_pred+k * (z[t]-x_pred)  # 推定更新
+        p=(1.0-k) * p_pred   # 分散更新
+
+        x_est[t]=x
+        p_est[t]=p
+        k_hist[t]=k
+    return x_est, p_est, k_hist
+
+#def predict(u, x0=0.0):
+#    x=float(x0)
+#    x_pred=np.zeros(len(u), dtype=float)
+#    for t in range(len(u)):
+#        x=x+u[t]
+#        x_pred[t]=x
+#    return x_pred
+
+def plot(t, x_true, z_meas, x_est):
     plt.figure()
     plt.plot(t, x_true, label="True position")
     plt.plot(t, z_meas, label="Measurement", linestyle="None", marker="o", markersize=3)
-    plt.plot(t, x_predict, label="prediction")
+    plt.plot(t, x_est, label="kalman estimate")
     plt.xlabel("time step")
     plt.ylabel("position")
-    plt.title("Prediction-True/Measurement")
+    plt.title("kalman estimate-True/Measurement")
     plt.legend()
     plt.tight_layout()
     plt.show()
@@ -50,12 +72,13 @@ def main():
     parser.add_argument("--q", type=float, default=0.2)
     parser.add_argument("--r", type=float, default=2.0)
     parser.add_argument("--x0", type=float, default=0.0)
+    parser.add_argument("--p0", type=float, default=0.0)
     args = parser.parse_args()
 
     u, x_true, z_meas = simulate_1d_robot(args.steps, args.seed, args.q, args.r)
-    x_predict = predict(u, x0=args.x0)
+    x_est, _, _ = kalman_filter(u, z_meas, x0=args.x0, p0=args.p0, q=args.q, r=args.r)
     t = np.arange(args.steps)
-    plot(t, x_true, z_meas, x_predict)
+    plot(t, x_true, z_meas, x_est)
 
 if __name__ == "__main__":
     main()
